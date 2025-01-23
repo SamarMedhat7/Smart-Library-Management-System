1f730e800ab3da9dea0dfd48918d3815980ffa94
SamarMedhat7 <samarmedhat573@gmail.com>
2025-01-23 07:11:31 +0200

    foundational structure for your Smart Library Management System

--- library/models.py
+++ library/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+    publication_year = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    rating = models.FloatField(default=0.0)
+
+class Borrow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrow_date = models.DateField(auto_now_add=True)
+    due_date = models.DateField()
+    returned = models.BooleanField(default=False)
+    fine = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
--- library/views.py
+++ library/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Book, Borrow
+from .serializers import BookSerializer, BorrowSerializer
 
-# Create your views here.
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BorrowViewSet(viewsets.ModelViewSet):
+    queryset = Borrow.objects.all()
+    serializer_class = BorrowSerializer
