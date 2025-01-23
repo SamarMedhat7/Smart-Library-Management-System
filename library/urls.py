dc820df062f52238bc2ac8a83d7e22335f4ef78a
SamarMedhat7 <samarmedhat573@gmail.com>
2025-01-23 07:36:41 +0200

    create user dashboard

--- library/urls.py
+++ library/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, BorrowViewSet
+from .views import BookViewSet, BorrowViewSet,user_dashboard
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
 router.register(r'borrows', BorrowViewSet)
+router.register(r'borrows', BorrowViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('dashboard', user_dashboard, name='user_dashboard_api'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
 from .models import Book, Borrow
 from .serializers import BookSerializer, BorrowSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
@@ -9,3 +12,16 @@ class BookViewSet(viewsets.ModelViewSet):
 class BorrowViewSet(viewsets.ModelViewSet):
     queryset = Borrow.objects.all()
     serializer_class = BorrowSerializer
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_dashboard(request):
+    user_borrows = Borrow.objects.filter(user=request.user)
+    serializer = BorrowSerializer(user_borrows, many=True)
+    
+    total_fine = sum(borrow.fine for borrow in user_borrows if not borrow.returned)
+
+    return Response({
+        'borrowing_history': serializer.data,
+        'total_fine': total_fine,
+    })    
