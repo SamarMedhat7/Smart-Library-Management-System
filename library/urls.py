a753b5e908bb95d708bfa6a02ef9da68ba2e0163
SamarMedhat7 <samarmedhat573@gmail.com>
2025-01-23 08:00:13 +0200

    apply search &filters&suggestions

--- library/urls.py
+++ library/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, BorrowViewSet,user_dashboard
+from .views import BookViewSet, BorrowViewSet,user_dashboard,search_books,search_suggestions
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
-router.register(r'borrows', BorrowViewSet)
 router.register(r'borrows', BorrowViewSet)
 
 
 urlpatterns = [
     path('', include(router.urls)),
     path('dashboard', user_dashboard, name='user_dashboard_api'),
+    path('books/search/', search_books, name='search_books'),
+    path('api/books/suggestions/', search_suggestions, name='search_suggestions'),
+
 ]
--- library/views.py
+++ library/views.py
@@ -4,6 +4,8 @@ from .serializers import BookSerializer, BorrowSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from django.db.models import Q
+
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
@@ -25,3 +27,51 @@ def user_dashboard(request):
         'borrowing_history': serializer.data,
         'total_fine': total_fine,
     })    
+
+
+
+@api_view(['GET'])
+def search_books(request):
+    query = request.GET.get('q', '')
+    availability = request.GET.get('availability', None)
+    publication_year = request.GET.get('publication_year', None)
+    rating = request.GET.get('rating', None)
+    genres = request.GET.getlist('genres', [])
+    authors = request.GET.getlist('authors', [])
+
+    filters = Q()
+
+    if query:
+        filters &= Q(title__icontains=query) | Q(author__icontains=query) | Q(genre__icontains=query)
+
+    if availability is not None:
+        filters &= Q(is_available=availability.lower() == 'true')
+
+    if publication_year:
+        filters &= Q(publication_year=publication_year)
+
+    if rating:
+        filters &= Q(rating__gte=rating)
+
+    if genres:
+        filters &= Q(genre__in=genres)
+
+    if authors:
+        filters &= Q(author__in=authors)
+    books = Book.objects.filter(filters)
+    serializer = BookSerializer(books, many=True)
+
+    return Response(serializer.data)    
+
+
+@api_view(['GET'])
+def search_suggestions(request):
+    query = request.GET.get('q', '')
+    suggestions = []
+
+    if query:
+        suggestions = Book.objects.filter(Q(title__icontains=query) | Q(author__icontains=query)).distinct()[:5]
+
+    suggestion_data = [{'title': book.title, 'author': book.author} for book in suggestions]
+
+    return Response(suggestion_data)
